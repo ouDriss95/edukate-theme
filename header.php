026f76f89094ad83ea91951ba4835607c405cfb8
ouDriss95 <driss.oudmine@gmail.com>
2022-10-23 16:16:27 +0100

    Make pages dynamic

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
   <?php wp_head(); ?>
 </head>
 
-<body>
+<body <?php body_class(); ?>>
   <!-- Topbar Start -->
   <div class="container-fluid bg-dark">
     <div class="row py-2 px-lg-5">
@@ -16,6 +16,7 @@
           <small><i class="fa fa-envelope mr-2"></i>info@example.com</small>
         </div>
       </div>
+
       <div class="col-lg-6 text-center text-lg-right">
         <div class="d-inline-flex align-items-center">
           <a class="text-white px-2" href="">
@@ -42,7 +43,7 @@
   <!-- Navbar Start -->
   <div class="container-fluid p-0">
     <nav class="navbar navbar-expand-lg bg-white navbar-light py-3 py-lg-0 px-lg-5">
-      <a href="index.html" class="navbar-brand ml-lg-3">
+      <a href="<?php echo site_url() ?>" class="navbar-brand ml-lg-3">
         <h1 class="m-0 text-uppercase text-primary">
           <i class="fa fa-book-reader mr-3"></i>Edukate
         </h1>
@@ -50,11 +51,12 @@
       <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
         <span class="navbar-toggler-icon"></span>
       </button>
+
       <div class="collapse navbar-collapse justify-content-between px-lg-3" id="navbarCollapse">
         <div class="navbar-nav mx-auto py-0">
           <a href="index.html" class="nav-item nav-link active">Home</a>
-          <a href="about.html" class="nav-item nav-link">About</a>
-          <a href="course.html" class="nav-item nav-link">Courses</a>
+          <a href="<?php echo site_url('/about-us') ?>" class="nav-item nav-link">About us</a>
+          <a href="<?php echo site_url('/sample-page') ?>" class="nav-item nav-link">Courses</a>
           <div class="nav-item dropdown">
             <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Pages</a>
             <div class="dropdown-menu m-0">
@@ -71,30 +73,3 @@
     </nav>
   </div>
   <!-- Navbar End -->
-
-  <!-- Header Start -->
-  <div class="jumbotron jumbotron-fluid position-relative overlay-bottom" style="margin-bottom: 90px">
-    <div class="container text-center my-5 py-5">
-      <h1 class="text-white mt-4 mb-4">Learn From Home</h1>
-      <h1 class="text-white display-1 mb-5">Education Courses</h1>
-      <div class="mx-auto mb-5" style="width: 100%; max-width: 600px">
-        <div class="input-group">
-          <div class="input-group-prepend">
-            <button class="btn btn-outline-light bg-white text-body px-4 dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Courses
-            </button>
-            <div class="dropdown-menu">
-              <a class="dropdown-item" href="#">Courses 1</a>
-              <a class="dropdown-item" href="#">Courses 2</a>
-              <a class="dropdown-item" href="#">Courses 3</a>
-            </div>
-          </div>
-          <input type="text" class="form-control border-light" style="padding: 30px 25px" placeholder="Keyword" />
-          <div class="input-group-append">
-            <button class="btn btn-secondary px-4 px-lg-5">Search</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- Header End -->
--- page.php
+++ page.php
@@ -5,7 +5,133 @@ get_header();
 while (have_posts()) {
   the_post(); ?>
 
-  
+  <!-- Header Section -->
+  <div class="jumbotron jumbotron-fluid page-header position-relative overlay-bottom" style="margin-bottom: 90px;">
+    <div class="container text-center py-5">
+      <h1 class="text-white display-1"><?php the_title(); ?></h1>
+
+
+
+      <?php
+      $theParent = wp_get_post_parent_id(get_the_ID());
+      if ($theParent) { ?>
+
+        <div class="d-inline-flex text-white mb-5">
+          <p class="m-0 text-uppercase"><a class="text-white" href="<?php echo get_permalink($theParent); ?>"><?php echo get_the_title($theParent); ?></a></p>
+          <i class="fa fa-angle-double-right pt-1 px-3"></i>
+          <p class="m-0 text-uppercase"><?php the_title(); ?></p>
+        </div>
+
+      <?php } ?>
+
+      <div class="mx-auto mb-5" style="width: 100%; max-width: 600px;">
+        <div class="input-group">
+
+          <input type="text" class="form-control border-light" style="padding: 30px 25px;" placeholder="Keyword">
+          <div class="input-group-append">
+            <button class="btn btn-secondary px-4 px-lg-5">Search</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+  <!-- About Section -->
+  <div class="container-fluid py-5">
+    <div class="container py-5">
+      <div class="row">
+        <div class="col-lg-9">
+          <div class="section-title position-relative mb-4">
+            <h6 class="d-inline-block position-relative text-secondary text-uppercase pb-2"><?php the_title(); ?></h6>
+
+          </div>
+          <?php the_content(); ?>
+
+        </div>
+
+
+
+        <?php
+        $testArray = get_pages(array(
+          'child_of' => get_the_ID()
+        ));
+
+        if ($theParent or $testArray) { ?>
+          <div class="col-lg-3">
+            <div class="page-links">
+              <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent); ?>"><?php echo get_the_title($theParent); ?></a></h2>
+              <ul class="min-list">
+                <?php
+                if ($theParent) {
+                  $findChildrenOf = $theParent;
+                } else {
+                  $findChildrenOf = get_the_ID();
+                }
+
+                wp_list_pages(array(
+                  'title_li' => NULL,
+                  'child_of' => $findChildrenOf
+                ));
+                ?>
+              </ul>
+            </div>
+          </div>
+        <?php } ?>
+      </div>
+    </div>
+  </div>
+
+
+
+  <!-- Feature Section -->
+  <div class="container-fluid py-5">
+    <div class="container py-5">
+      <div class="row">
+        <div class="col-lg-7 mb-5 mb-lg-0">
+          <div class="section-title position-relative mb-4">
+            <h6 class="d-inline-block position-relative text-secondary text-uppercase pb-2">Why Choose Us?</h6>
+            <h1 class="display-4">Why You Should Start Learning with Us?</h1>
+          </div>
+          <p class="mb-4 pb-2">Aliquyam accusam clita nonumy ipsum sit sea clita ipsum clita, ipsum dolores amet voluptua duo dolores et sit ipsum rebum, sadipscing et erat eirmod diam kasd labore clita est. Diam sanctus gubergren sit rebum clita amet.</p>
+          <div class="d-flex mb-3">
+            <div class="btn-icon bg-primary mr-4">
+              <i class="fa fa-2x fa-graduation-cap text-white"></i>
+            </div>
+            <div class="mt-n1">
+              <h4>Skilled Instructors</h4>
+              <p>Labore rebum duo est Sit dolore eos sit tempor eos stet, vero vero clita magna kasd no nonumy et eos dolor magna ipsum.</p>
+            </div>
+          </div>
+          <div class="d-flex mb-3">
+            <div class="btn-icon bg-secondary mr-4">
+              <i class="fa fa-2x fa-certificate text-white"></i>
+            </div>
+            <div class="mt-n1">
+              <h4>International Certificate</h4>
+              <p>Labore rebum duo est Sit dolore eos sit tempor eos stet, vero vero clita magna kasd no nonumy et eos dolor magna ipsum.</p>
+            </div>
+          </div>
+          <div class="d-flex">
+            <div class="btn-icon bg-warning mr-4">
+              <i class="fa fa-2x fa-book-reader text-white"></i>
+            </div>
+            <div class="mt-n1">
+              <h4>Online Classes</h4>
+              <p class="m-0">Labore rebum duo est Sit dolore eos sit tempor eos stet, vero vero clita magna kasd no nonumy et eos dolor magna ipsum.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-5" style="min-height: 500px;">
+          <div class="position-relative h-100">
+            <img class="position-absolute w-100 h-100" src="img/feature.jpg" style="object-fit: cover;">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
 
 <?php }
 
